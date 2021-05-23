2a12e5091a5c98e5f342da782a4d79e6bb8d45d8
amit-c-ai <amitchoudhary9425@gmail.com>
2021-05-23 18:51:05 +0530

    successfully worked on localhost

--- client.py
+++ client.py
@@ -1,40 +1,41 @@
-# Python program to implement client side of chat room.
 import socket
-import select
-import sys
+import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.connect((IP_address, Port))
+#entering nickname
+nickname = input("Choose your nickname: ")
 
-while True:
+#connecting to server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = input("Enter exact address at which server is running: ")
+port = int(input("Enter exact port at which server is running: "))
+client.connect((address, port))
 
-	# maintains a list of possible input streams
-	sockets_list = [sys.stdin, server]
+#listening to server and sending nickname
+def receive():
+    while(True):
+        try:
+            # recieve message from server
+            # if 'NICK' then send nickname
+            message = client.recv(1024).decode('ascii')
+            if(message == "NICK"):
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            #close connection when error
+            print("An error occured!")
+            client.close()
+            break
 
-	""" There are two possible input situations. Either the
-	user wants to give manual input to send to other people,
-	or the server is sending a message to be printed on the
-	screen. Select returns from sockets_list, the stream that
-	is reader for input. So for example, if the server wants
-	to send a message, then the if condition will hold true
-	below.If the user wants to send a message, the else
-	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+# sending messages to server
+def write():
+    while(True):
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
 
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print (message)
-		else:
-			message = sys.stdin.readline()
-			server.send(message)
-			sys.stdout.write("<You>")
-			sys.stdout.write(message)
-			sys.stdout.flush()
-server.close()
+#starting threads for listening and writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
+write_thread = threading.Thread(target=write)
+write_thread.start()
